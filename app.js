0238ecbff3e8f2c2976a9dc8dbf07057856b6977
DevMD <81987904+Cyborg-CS@users.noreply.github.com>
2021-12-13 21:33:34 +0530

    nodes taken

---
+++ app.js
@@ -0,0 +1,10 @@
+const userBirthday = document.querySelector("#birthday");
+const userLuckynum = document.querySelector("lucky-number");
+const checkButton = document.querySelector("check-btn");
+const resultArea = document.querySelector("#result-box");
+
+checkButton.addEventListener("click", function clickHandler(){
+  console.log('Hello world');
+});
+
+// function clickHandler()
