9a9ddb43d9a885b9f7d13a36455708b5889a75ef
DevMD <81987904+Cyborg-CS@users.noreply.github.com>
2021-12-13 22:24:23 +0530

    validation function

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
 const userBirthday = document.querySelector("#birthday");
-const userLuckynum = document.querySelector("lucky-number");
-const checkButton = document.querySelector("check-btn");
+const userLuckynum = document.querySelector("#lucky-number");
+const checkButton = document.querySelector("#check-btn");
 const resultArea = document.querySelector("#result-box");
+const errorMessage = document.querySelector("#error-message");
 
-checkButton.addEventListener("click", function clickHandler(){
-  console.log('Hello world');
-});
+checkButton.addEventListener("click", verifyBirthdayAndLuckyNumber()); 
 
-// function clickHandler()
+function verifyBirthdayAndLuckyNumber(){
+    hideMessage();
+    if(userLuckynum.value = null ){
+        
+    }else{
+        showMessage("fill your date of birth !")
+    };
+};
+
+
+function hideMessage() {
+    errorMessage.style.display = "none";
+}
+
+function showMessage(message) {
+    errorMessage.style.display = "block";
+    errorMessage.innerText = message;
+}
+// function hideMessage(){
+//   errorMessage.style.display = "none";
+// };
+
+// function showMessage(message){
+//     errorMessage.style.display = "block";
+//     errorMessage.innerText = message;
+// }
+
+
+ 
+
+
